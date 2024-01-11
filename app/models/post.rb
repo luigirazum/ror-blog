135cbf7547c6076513ab0799b80a5d0fbdcebb76
Luis Zubia <soyzubia@gmail.com>
2024-01-11 05:49:31 -0700

    Setting associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id', inverse_of: 'posts', counter_cache: :posts_counter
+  has_many :comments
+  has_many :likes
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  has_many :posts, foreign_key: 'author_id', inverse_of: 'author'
+  has_many :comments
+  has_many :likes
 end
