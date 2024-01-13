ef9038c5115d78b083dfe02610cd9a38353dca6c
Luis Zubia <soyzubia@gmail.com>
2024-01-12 18:44:59 -0700

    Specs for 'Like': check like associations

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -45,4 +45,20 @@ RSpec.describe Like, type: :model do
       end
     end
   end
+
+  describe '* associations' do
+    context '.user' do
+      it "=> responds for belongs to an 'user'" do
+        association = described_class.reflect_on_association(:user)
+        expect(association.macro).to eq(:belongs_to)
+      end
+    end
+
+    context '.post' do
+      it "=> responds for belongs to a 'post'" do
+        association = described_class.reflect_on_association(:post)
+        expect(association.macro).to eq(:belongs_to)
+      end
+    end
+  end
 end
