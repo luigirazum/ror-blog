7e450e263e6978acd9ccb597d00a7949aaa9be05
Luis Zubia <soyzubia@gmail.com>
2024-01-12 18:21:12 -0700

    Specs for 'User': check user attributes

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,4 +14,32 @@ RSpec.describe User, type: :model do
       expect(attr_mod({ name: nil })).to_not be_valid
     end
   end
+
+  describe '* attributes' do
+    describe '.name validations' do
+      it '- must be provided' do
+        expect(attr_mod({ name: nil })).to_not be_valid
+      end
+
+      it '- must be 80 characters max' do
+        expect(attr_mod({ name: 'a' * 81 })).to_not be_valid
+      end
+    end
+
+    describe '.posts_counter validations' do
+      it '- is an <Integer>' do
+        expect(attr_mod({ posts_counter: 'a' })).to_not be_valid
+        expect(attr_mod({ posts_counter: nil })).to_not be_valid
+        expect(attr_mod({ posts_counter: true })).to_not be_valid
+        expect(attr_mod({ posts_counter: 10 })).to be_valid
+      end
+
+      it '- is greater than or equal to zero' do
+        expect(attr_mod({ posts_counter: -1 })).to_not be_valid
+        expect(attr_mod({ posts_counter: -10 })).to_not be_valid
+        expect(attr_mod({ posts_counter: 0 })).to be_valid
+        expect(attr_mod({ posts_counter: 5 })).to be_valid
+      end
+    end
+  end
 end
