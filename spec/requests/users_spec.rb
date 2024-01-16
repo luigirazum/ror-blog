9b1258ab4f0ccfdef9685ed7194fc2d0d834ac66
Luis Zubia <soyzubia@gmail.com>
2024-01-16 03:53:58 -0700

    controller specs for 'Users': check 'status' for 'index' action

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,16 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe 'Users', type: :request do
-  describe 'GET /index' do
-    it 'returns http success' do
-      get '/users/index'
-      expect(response).to have_http_status(:success)
+RSpec.describe "'Users' - [Controller]", type: :request do
+  describe "'GET /index' => 'index' action at 'Users' controller" do
+    before { get users_path }
+
+    context "* 'status'", :status do
+      it 'returns http success' do
+        expect(response).to have_http_status(:success)
+      end
     end
   end
 
   describe 'GET /show' do
     it 'returns http success' do
-      get '/users/show'
+      get '/users/1'
       expect(response).to have_http_status(:success)
     end
   end
