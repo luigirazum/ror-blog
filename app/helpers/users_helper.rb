4aa3aa00ce2e5101119272c2f3cc10ce990d435e
Luis Zubia <soyzubia@gmail.com>
2024-01-19 23:15:17 -0700

    Add logic to render a 'no posts' message when the user has no posts

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+  def render_posts
+    if @posts.size.positive?
+      render partial: 'posts/post', layout: 'post_link', collection: @posts
+    else
+      render partial: 'empty', locals: { options: %w[user posts] }
+    end
+  end
 end
