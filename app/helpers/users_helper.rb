5e549ead7586627ac8826d4c1aebff1e56c03629
Luis Zubia <soyzubia@gmail.com>
2024-01-20 23:24:11 -0700

    Rename layout template from link to border

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,7 @@
 module UsersHelper
   def render_posts
     if @posts.size.positive?
-      render partial: 'posts/post', layout: 'post_link', collection: @posts
+      render partial: 'posts/post', layout: 'post_border', collection: @posts
     else
       render partial: 'empty', locals: { options: %w[user posts] }
     end
