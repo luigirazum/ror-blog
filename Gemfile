da780b8e5475a8953c7224ce81bc8c5d106d3f77
Luis Zubia <soyzubia@gmail.com>
2024-02-02 03:56:10 -0700

    Fix rubocop linters errors

--- Gemfile
+++ Gemfile
@@ -51,7 +51,7 @@ gem 'bootsnap', require: false
 # gem "image_processing", "~> 1.2"
 
 # Add authentication with 'devise' gem
-gem "devise", "~> 4.9"
+gem 'devise', '~> 4.9'
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
-  
+
   # associations
   has_many :posts, foreign_key: 'author_id', inverse_of: :author
   has_many :comments, inverse_of: :user
