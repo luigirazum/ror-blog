b09dfbd4b7ca7a41b2750e396401ada71b735721
Porag Sarkar <hello2sarkar@outlook.com>
2024-02-02 03:15:14 -0600

    Add 'devise' to 'user' model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  # Add 'devise' to users
+  # devise_for :users
+  devise_for :users
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :posts, only: %i[new create], constraints: { id: /[0-9]+/ } do
     resources :comments, only: %i[new create]
