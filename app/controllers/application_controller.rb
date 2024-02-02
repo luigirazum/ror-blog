ab927317a36ff70752db6f8cb1371d4d5ebc6364
Luis Zubia <soyzubia@gmail.com>
2024-02-02 03:52:57 -0700

    Add parameter sanitizer (strong params) for 'devise'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,22 @@ class ApplicationController < ActionController::Base
   include Pagy::Backend
 
   before_action :authenticate_user!
+  protect_from_forgery with: :exception
+  before_action :update_allowed_parameters, if: :devise_controller?
+
+  protected
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_in) do |user_params|
+      user_params.permit(:email, :password)
+    end
+
+    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+      user_params.permit(:name, :email, :password, :password_confirmation)
+    end
+
+    devise_parameter_sanitizer.permit(:account_update) do |user_params|
+      user_params.permit(:name, :email, :password, :current_password)
+    end
+  end
 end
