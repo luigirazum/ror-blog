da9f95c32803f125c83d416af82b159533635b9c
Porag Sarkar <hello2sarkar@outlook.com>
2024-02-03 04:55:46 -0600

    Add routes for delete actions on 'Post's and 'Comment's

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :posts, only: %i[new create], constraints: { id: /[0-9]+/ } do
+  resources :posts, only: %i[new create destroy], constraints: { id: /[0-9]+/ } do
     resources :comments, only: %i[new create]
     resources :likes, only: %i[create]
   end
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   resources :users, only: %i[index show], constraints: { id: /[0-9]+/ } do
     resources :posts, only: %i[index show]
   end
+
+  resources :comments, only: %i[destroy], constraints: { id: /[0-9]+/ }
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
