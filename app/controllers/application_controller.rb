1768f625547a7e1076ace7b801945ed89aa67c55
Porag Sarkar <hello2sarkar@outlook.com>
2024-02-03 12:11:50 -0600

    Include the role to the user strong params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,11 +13,11 @@ class ApplicationController < ActionController::Base
     end
 
     devise_parameter_sanitizer.permit(:sign_up) do |user_params|
-      user_params.permit(:name, :email, :password, :password_confirmation)
+      user_params.permit(:name, :email, :password, :password_confirmation, :role)
     end
 
     devise_parameter_sanitizer.permit(:account_update) do |user_params|
-      user_params.permit(:name, :email, :password, :current_password)
+      user_params.permit(:name, :email, :password, :current_password, :role)
     end
   end
 end
